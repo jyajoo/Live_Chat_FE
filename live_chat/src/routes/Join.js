6df5df52a5199ef3ddab4e6ddd66c7fec84e9c88
xogh8755 <xogh8755@naver.com>
2023-03-08 16:01:04 +0900

    Join demo

--- live_chat/src/routes/Join.js
+++ live_chat/src/routes/Join.js
@@ -3,12 +3,19 @@ import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
 import { useEffect, useState } from "react";
-import styles from "../css/Join.module.css";
+import { useHistory, useNavigate } from "react-router-dom";
 
+import styles from "../css/Join.module.css";
+import baseStyles from "../css/Base.module.css";
+import axios from "axios";
+import { FormLabel } from "react-bootstrap";
+import { json } from "react-router";
 function Join() {
+  const navigate = useNavigate();
   const [id, setId] = useState("");
   const [pw, setPw] = useState("");
   const [nickName, setNickName] = useState("");
+
   const onChangeId = (event) => {
     setId(event.target.value);
   };
@@ -18,40 +25,69 @@ function Join() {
   const onChangeNickname = (event) => {
     setNickName(event.target.value);
   };
-  return (
-    <div>
-      <h2 id={styles.title}>Join</h2>
-      <Form>
-        <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Id</Form.Label>
-            <Form.Control
-              onChange={onChangeId}
-              value={id}
-              type="id"
-              placeholder="Enter Id"
-            />
-          </Form.Group>
+  let data = {
+    userId: id,
+    userPw: pw,
+    userNickName: nickName,
+  };
+  const handleJoin = (e) => {
+    e.preventDefault();
+    console.log("this----------------------");
+    console.log(data);
+    axios
+      .post("/join", data)
+      .then((response) => {
+        console.log("response.data----------------------");
 
-          <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control
-              onChange={onChangePw}
-              value={pw}
-              type="password"
-              placeholder="Password"
-            />
-          </Form.Group>
-        </Row>
+        console.log(response);
+        if (response.status === 200) {
+          alert(response.data +"님 회원가입 성공~");
+          navigate("/");
+        }
+
+        // response
+      })
+      .catch((error) => {
+        // 오류발생시 실행
+        console.log("error!!!!!!");
+      })
+      .then(function () {
+        
+      });
+  };
+  return (
+    <div id={baseStyles.content}>
+      <h2 id={baseStyles.title}>Join</h2>
+      <Form onSubmit={handleJoin}>
+        <Form.Group className="mb-3" controlId="formGridId">
+          <Form.Label>Id</Form.Label>
+          <Form.Control
+            onChange={onChangeId}
+            value={id}
+            type="id"
+            placeholder="Enter Id"
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formGridPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            onChange={onChangePw}
+            value={pw}
+            type="password"
+            placeholder="Password"
+          />
+        </Form.Group>
 
         <Form.Group className="mb-3" controlId="formGridAddress1">
           <Form.Label>Nickname</Form.Label>
           <Form.Control onChange={onChangeNickname} value={nickName} />
         </Form.Group>
-
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+        <div className="d-grid gap-2">
+          <Button id={styles.btn} variant="primary" type="submit">
+            Submit
+          </Button>
+        </div>
       </Form>
     </div>
   );
--- live_chat/src/routes/Login.js
+++ live_chat/src/routes/Login.js
@@ -1,25 +1,28 @@
+import {Link} from "react-router-dom"
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import styles from "../css/Login.module.css"
+import styles from "../css/Login.module.css";
 function Login() {
   return (
     <div>
       <h2 id={styles.title}>LOGIN</h2>
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>ID</Form.Label>
-        <Form.Control type="email" placeholder="Enter Id" />
-      </Form.Group>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>ID</Form.Label>
+          <Form.Control type="email" placeholder="Enter Id" />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <div className="d-grid gap-3">
+          <Button variant="primary" type="submit">
+            Login
+          </Button>
+        </div>
+      </Form>
+      <Link to = "/join">회원가입</Link>
     </div>
   );
 }
